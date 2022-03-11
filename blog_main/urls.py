bce4bcc3e5ffd86a6141e1f02746cf3f133367c6
cheon4050 <ehdcjs159@gmail.com>
2022-03-12 00:01:45 +0900

    blog 앱 및 post 모델 생성

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -1,4 +1,4 @@
-"""do_it_django_prj URL Configuration
+"""blog_main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
