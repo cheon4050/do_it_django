65fd3253e5b3ab7f7d90ef17b8e78e7bf8f7be0d
cheon4050 <ehdcjs159@gmail.com>
2022-04-08 17:22:45 +0900

    카테고리 뷰 생성, 태그 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,21 @@ from django.db import models
 import os
 # Create your models here.
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}'
     class Meta:
         verbose_name_plural = 'categories'
 
@@ -26,6 +36,7 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, blank=True,null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
     #methods
     def __str__(self):
         return f'[{self.pk}]  [{self.title}] :: {self.author}'
--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     # path('<int:pk>/', views.single_post_page),
     # path('', views.index),
-    path('<int:pk>/', views.PostDetail.as_view())
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.show_category_posts)
 ]
