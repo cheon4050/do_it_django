693e6c4e4a0b5f4ad3b340b0b01a0d51a55022ef
cheon4050 <ehdcjs159@gmail.com>
2022-04-29 17:28:36 +0900

    comment 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -14,3 +13,4 @@ class TagAdmin(admin.ModelAdmin):
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, MarkdownxModelAdmin)
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(Comment)
